9f9f737eb356e21377af21b73d11724f56a2e37d
Akayxn <yujansharma20621129@gmail.com>
2026-05-01 19:14:20 +0545

    completed the security config with the configuration of securityFilterchain like disabling the csrf, allowing clients to access the /auth/ without any authorzation and adding our custom jwt filter before the usernamePasswordAuthFilter

--- src/main/java/com/akayxn/cryptocurrencymarkettracker/configuration/SecurityConfig.java
+++ src/main/java/com/akayxn/cryptocurrencymarkettracker/configuration/SecurityConfig.java
@@ -1,8 +1,51 @@
 package com.akayxn.cryptocurrencymarkettracker.configuration;
 
+import com.akayxn.cryptocurrencymarkettracker.service.UserDetailsServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final UserDetailsServiceImpl userDetailsService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // we are disabling the csrf because we are using JWTs and the JWTs are stateless
+         return http.csrf(AbstractHttpConfigurer::disable)
+                 // in the first and second line is basically letting people access without any permission
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll()
+                        // and anyRequest.aunenticated basically means that any other request must be logged in
+                        // before sending a request
+                        .anyRequest().authenticated())
+//                we are basically adding our customer filter to handle logins before the UsernamePasswordAuthFilter
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+//                 then we build it
+                .build();
+    }
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
